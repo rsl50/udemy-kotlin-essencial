93451fdb47088172b411f00701e043849bf2753d
Robson Lima <rsl.robson@gmail.com>
2021-08-03 18:08:22 -0300

    Learning about constants in Kotlin.

---
+++ src/main/kotlin/fundamentos/Const.kt
@@ -0,0 +1,9 @@
+package fundamentos
+
+fun main() {
+    val a : Int = 1
+    val b = 2 // Tipo Inferido
+
+    // a = a + b // Não é possível fazer a atribuição já que o tipo 'val' indica uma constante.
+    print(a)
+}
---
+++ src/main/kotlin/fundamentos/ConstJava.kt
@@ -0,0 +1,6 @@
+package fundamentos
+
+fun main() {
+    val raio = 4.5
+    print(raio * raio * Math.PI) // Acessando a constante PI direto do Java.
+}
