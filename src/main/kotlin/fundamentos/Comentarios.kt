0956756f948f2a42a1646b3e38dd42f82eb7237b
Robson Lima <rsl.robson@gmail.com>
2021-08-03 17:56:16 -0300

    Using comments in Kotlin.

---
+++ src/main/kotlin/fundamentos/Comentarios.kt
@@ -0,0 +1,25 @@
+package fundamentos
+
+/**
+ * Só para dizer que o Kotlin suporta o KDoc... :)
+ * Pode-se exportar a documentação usando o Dokka: https://github.com/Kotlin/dokka
+ *
+ * @param args Lista de parâmetros passados por linha de comando
+ */
+fun main(args: Array<String>) {
+    // Comentário de uma linha
+    println("Opa")
+
+    /*
+      Mais de
+      uma linha
+     */
+    println("Sério?")
+
+    /*
+     * Mais de
+     * uma linha
+     * com estilo
+     */
+    println("Legal!")
+}
