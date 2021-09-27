062b203eea1c401a8a4c232d0a90445db4d07390
Robson Lima <rsl.robson@gmail.com>
2021-09-26 22:06:56 -0300

    Understanding Enums in Kotlin.

---
+++ src/main/kotlin/classes/Enum1.kt
@@ -0,0 +1,9 @@
+package classes
+
+enum class DiaSemana {
+    DOMINGO, SEGUNDA, TERÇA, QUARTA, QUINTA, SEXTA, SABADO
+}
+
+fun main() {
+    println("O melhor dia da semana é ${DiaSemana.SABADO}.")
+}
