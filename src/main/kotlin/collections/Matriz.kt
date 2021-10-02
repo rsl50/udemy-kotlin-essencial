a1de560b26177b0f64e3a5d0ed40d2b2e06f868e
Robson Lima <rsl.robson@gmail.com>
2021-10-02 17:25:01 -0300

    Using Matrix in Kotlin.

---
+++ src/main/kotlin/collections/Matriz.kt
@@ -0,0 +1,28 @@
+package collections
+
+fun main() {
+    val matriz = Array(3) { arrayOfNulls<Int>(3)} // 3X3 matrix
+
+    // Initializing the array
+    matriz[0][0] = 1
+    matriz[0][1] = 2
+    matriz[0][2] = 3
+    matriz[1][0] = 4
+    matriz[1][1] = 5
+    matriz[1][2] = 6
+    matriz[2][0] = 7
+    matriz[2][1] = 8
+    matriz[2][2] = 9 // This could also be done using matriz.get(2).set(2, 9)
+
+    for ((linha, linhaArray) in matriz.withIndex()) {
+        for ((coluna, valor) in linhaArray.withIndex()) {
+            println("[$linha][$coluna] - ${matriz[linha][coluna]} == $valor")
+        }
+    }
+
+    matriz.forEach { linha ->
+        linha.forEach { valor ->
+            println(valor)
+        }
+    }
+}
